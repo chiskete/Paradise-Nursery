1b2d323690d887a4f3e4e333a37b4b31b2f90695
chiskete <tronoacosta24@gmail.com>
2025-03-12 18:56:06 -0600

    Primera subida

---
+++ Paradise-Nursery/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><a href=""></a>logo<img src="" alt="" /></li>
+                    <li><a href="">Products</a></li>
+                    <li><a href=""></a>carrito<img src="" alt="" /></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
---
+++ Paradise-Nursery/src/components/ProductTarget.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProductTarget() {
+    return(
+        <div className="tarjeta-producto">
+
+        </div>
+    )
+}
+
+export default ProductTarget;
